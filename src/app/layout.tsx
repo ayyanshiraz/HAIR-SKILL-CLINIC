22938f81b54cc6879bcbed17401ed6efdfa4ac07
YASIR IRSHAD <yasir.blackzero@gmail.com>
2026-07-13 13:20:35 +0500

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import CookieBanner from "@/components/CookieBanner";
 import { CartProvider } from "../context/CartContext";
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -33,6 +34,7 @@ export default function RootLayout({
         </main>
         <Footer />
         <CookieBanner />
+        <Analytics/>
       </body>
     </html>
   );
