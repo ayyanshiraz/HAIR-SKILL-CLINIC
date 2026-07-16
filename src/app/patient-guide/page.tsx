eb26274d6b32699f0d75fa362e22a09dd76ce1b7
YASIR IRSHAD <yasir.blackzero@gmail.com>
2026-07-16 16:04:22 +0500

    update

--- src/app/patient-guide/page.tsx
+++ src/app/patient-guide/page.tsx
@@ -12,6 +12,14 @@ export const metadata: Metadata = {
     siteName: "Hair Skill Clinic",
     locale: "en_US",
     type: "website",
+    images: [
+      {
+        url: "https://www.hairskill.com/_next/image?url=%2Fhome%2Fowner1.webp&w=1080&q=75",
+        width: 1200,
+        height: 630,
+        alt: "Patient Guide | Hair Skill Clinic Pakistan",
+      },
+    ],
   },
 };
 
