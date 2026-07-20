3a1b53322b0c6eb7e65aa19acc0f30a08e87643f
YASIR IRSHAD <yasir.blackzero@gmail.com>
2026-07-20 11:34:59 +0500

    update

--- src/app/hair-transplant/treatments/afro-hair/page.tsx
+++ src/app/hair-transplant/treatments/afro-hair/page.tsx
@@ -14,6 +14,14 @@ export const metadata: Metadata = {
     siteName: "Hair Skill Clinic",
     locale: "en_US",
     type: "website",
+    images: [
+      {
+        url: "https://www.hairskill.com/hair-transplant/treatments/afro-hair/1.webp",
+        width: 1200,
+        height: 630,
+        alt: "Afro Hair Transplant and Coiled Hair Restoration at Hair Skill Clinic Lahore Pakistan",
+      },
+    ],
   },
 };
 
