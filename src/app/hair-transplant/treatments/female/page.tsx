bc7fd5e095fd4ff54211fee5cbe611bf0f6622b6
YASIR IRSHAD <yasir.blackzero@gmail.com>
2026-07-20 12:13:56 +0500

    update

--- src/app/hair-transplant/treatments/female/page.tsx
+++ src/app/hair-transplant/treatments/female/page.tsx
@@ -14,6 +14,14 @@ export const metadata: Metadata = {
     siteName: "Hair Skill Clinic",
     locale: "en_US",
     type: "website",
+    images: [
+      {
+        url: "https://www.hairskill.com/hair-transplant/21.webp",
+        width: 1200,
+        height: 630,
+        alt: "Female patient examining pre surgical hairline markings inside mirror at Hair Skill Clinic",
+      },
+    ],
   },
 };
 
